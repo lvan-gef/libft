7be2d9e0b59d78a835962a2d01fde292417d0dc0
luuk <lvan-gef@student.codam.nl>
2024-07-07 16:47:03 +0200

    Remove code that was incorrect

--- src/str/ft_strncmp.c
+++ src/str/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lvan-gef <lvan-gef@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/18 13:00:15 by lvan-gef      #+#    #+#                 */
-/*   Updated: 2024/06/08 19:19:51 by lvan-gef      ########   odam.nl         */
+/*   Updated: 2024/07/07 16:46:21 by lvan-gef      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	s1_ = (unsigned char *)s1;
 	s2_ = (unsigned char *)s2;
 	index = 0;
-	if (s1_ == s2_)
-		return (0);
 	while (index < n)
 	{
 		if (s1_[index] == '\0' && s2_[index] == '\0')
