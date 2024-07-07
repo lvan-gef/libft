b8fab8e3fe3900daed96d2a0da30aaaa4cd1cf72
luuk <lvan-gef@student.codam.nl>
2024-07-07 04:19:10 +0200

    Replaced 0 with NULL

--- src/str/ft_strjoin.c
+++ src/str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: lvan-gef <lvan-gef@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/20 16:01:04 by lvan-gef      #+#    #+#                 */
-/*   Updated: 2024/05/04 23:17:21 by lvan-gef      ########   odam.nl         */
+/*   Updated: 2024/07/07 04:18:44 by lvan-gef      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str_index = 0;
 	str = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
 	if (!str)
-		return (0);
+		return (NULL);
 	str_index = add_to(str, s1, str_index);
 	str_index = add_to(str, s2, str_index);
 	str[str_index] = '\0';
